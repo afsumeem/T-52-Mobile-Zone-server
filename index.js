559e3badeed9c77eeabc83c653d27f2276c4eb06
afsumeem <afsumeem@gmail.com>
2022-01-02 02:50:33 +0600

    detail

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
         });
 
         //get api- product details
-        app.get('/products/:_id', async (req, res) => {
+        app.get('/products/:id', async (req, res) => {
             const productDetail = await productCollection.findOne({ _id: ObjectId(req.params.id) });
             res.send(productDetail);
         })
